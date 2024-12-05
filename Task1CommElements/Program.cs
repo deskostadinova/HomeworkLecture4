a20d787c696b20d444e7d9b1491d0f39f6997f8a
deskostadinova <91456206+deskostadinova@users.noreply.github.com>
2024-12-05 13:20:43 +0200

    Task 2

--- Task1CommElements/Program.cs
+++ Task1CommElements/Program.cs
@@ -3,5 +3,5 @@
 string [] list1 = Console.ReadLine().Split(' ');
 string [] list2 = Console.ReadLine().Split(' ');
 
-var listCommon = list1.Intersect(list2);
+var listCommon = list1.Intersect(list2).ToArray();
 foreach (string s in listCommon) Console.WriteLine(s);
