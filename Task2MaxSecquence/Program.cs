f1e613ee98f211514a9dc73c56df71b48134078f
deskostadinova <91456206+deskostadinova@users.noreply.github.com>
2024-12-05 13:26:47 +0200

    Task 1 and 2

---
+++ Task2MaxSecquence/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int[] userInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int counter = 1;
+int max = 0;
+int sequence = 0;
+for (int i = 0 ; i < userInput.Length - 1; i++)
+{
+    if (userInput[i] == userInput[i + 1])
+    {
+        counter++;                   
+    }
+    else
+    {                  
+        counter = 1;                   
+    }
+    if (counter > max)
+    {
+        max = counter;
+        sequence = userInput[i];
+    }             
+}
+for (int j = 1; j <= max; j++)
+{
+    Console.Write($"{sequence} ");
+}
