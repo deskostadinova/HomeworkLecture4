f2088b4ad8ba1623269d962402cba7eba482a5de
deskostadinova <91456206+deskostadinova@users.noreply.github.com>
2024-12-09 11:18:02 +0200

    Task5

---
+++ Task5CountCharsInString/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter a word or a sentence: ");
+string inputChars = Console.ReadLine();
+
+Dictionary<char, int> charCounts = new Dictionary<char, int>();
+
+foreach (char c in inputChars)
+{
+    if (c != ' ') 
+    {
+        if (charCounts.ContainsKey(c))
+        {
+            charCounts[c]++;
+        }
+        else
+        {
+            charCounts[c] = 1;
+        }
+    }
+}
+
+foreach (var entry in charCounts)
+{
+    Console.WriteLine($"{entry.Key} -> {entry.Value}");
+}
