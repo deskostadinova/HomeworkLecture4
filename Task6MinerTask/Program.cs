952dcb12d181ee72cba5ebb9dce4c6ac1f74c6a0
deskostadinova <91456206+deskostadinova@users.noreply.github.com>
2025-01-09 09:57:45 +0200

    Task 6 and 7

---
+++ Task6MinerTask/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Dictionary<string, long> resources = new Dictionary<string, long>();
+
+string command;
+int commandCounter = 1;
+
+while ((command = Console.ReadLine()) != "stop")
+{
+    if (commandCounter % 2 != 0)
+    {
+        string typeOfResource = command;
+        long quantityOfResources = long.Parse(Console.ReadLine());
+
+        if (resources.ContainsKey(typeOfResource))
+        {
+            resources[typeOfResource] += quantityOfResources;
+        }
+        else
+        {
+            resources[typeOfResource] = quantityOfResources;
+        }
+    }
+
+    commandCounter+= 2;
+}
+
+foreach (var element in resources)
+{
+    Console.WriteLine($"{element.Key} -> {element.Value}");
+}
+    
---
+++ Task7BasicStackOperations/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] inputParams = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int n = inputParams[0]; 
+int s = inputParams[1]; 
+int x = inputParams[2]; 
+
+int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Stack<int> stack = new Stack<int>();
+
+for (int i = 0; i < n; i++)
+{
+    stack.Push(elements[i]);
+}
+
+for (int i = 0; i < s && stack.Count > 0; i++)
+{
+    stack.Pop();
+}
+
+if (stack.Contains(x))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine(stack.Count > 0 ? stack.Min() : 0);
+}
